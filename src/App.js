dbde4bea7b150fc215ca5eb73d79b639ab5d7ab9
Almog Tavor <almogtavor@gmail.com>
2020-08-25 01:56:44 +0300

    Unlimited View Calendars Support
    Not finished. Support unlimited calendars, to help view and pick dates easily.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,45 @@ import CalendarConatiner from './containers/CalendarContainer';
 import LowerFooterContainer from './containers/LowerFooterContainer';
 import HeaderContainer from './containers/HeaderContainer';
 
+const DateRangePickerComponents = (props) => {
+  return (
+    <>
+    {[...Array(props.boardsNum).keys()].map((i) => {
+      console.log(i);
+      return (
+        <div 
+          key={i}
+          className="calendar-component" 
+          style={{"gridColumn": i + 1, "gridRow": (i + 1) % 3}}
+        >
+          <HeaderContainer 
+            language={props.language}
+          />
+          <CalendarConatiner
+            firstDayOfWeekIndex={props.firstDayOfWeekIndex}
+            startYear={props.startYear}
+            endYear={props.endYear}
+          />
+          <LowerFooterContainer/>
+        </div>
+        );
+      })}
+    </>
+  );
+};
+
 function DateRangePicker(props) {
+  console.log(props);
   return (
     <div className="App">
       <div className="date-range-picker">
-        <div className="calendar-component right">
-            <HeaderContainer 
-              language={props.language}
-            />
-            <CalendarConatiner
-              firstDayOfWeekIndex={props.firstDayOfWeekIndex}
-              startYear={props.startYear}
-              endYear={props.endYear}
-            />
-            <LowerFooterContainer/>
-        </div>
-        <div className="calendar-component left">
-            <HeaderContainer/>
-            <CalendarConatiner/>
-            <LowerFooterContainer/>
-        </div>
+        <DateRangePickerComponents 
+            language={props.language} 
+            startYear={props.startYear} 
+            endYear={props.endYear}
+            firstDayOfWeekIndex={props.firstDayOfWeekIndex}
+            boardsNum={props.boardsNum}
+        />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ ReactDOM.render(
             startYear={1970} 
             endYear={2030}
             firstDayOfWeekIndex={0}
+            boardsNum={8}
         />
     </Provider>,
     document.getElementById('root')
