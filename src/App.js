9b015f9e827d6bb482ea31aa34bb3972f6640be8
Almog Tavor <70065337+almogtavor@users.noreply.github.com>
2020-09-02 22:35:57 +0300

    Button Addition
    Added a button that will show and hide the calendar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import "./styles/button.css";
 import DateRangePickerContainer from './containers/DateRangePickerContainer';
 
 function App(props) {
+  const [ showCalendar, setShowCalendar ] = useState(false);
+
   return (
     <div className="App">
-        <div className="date-range-picker" style={{
+      <button className="button" onClick={() => setShowCalendar(!showCalendar)}>
+        YYYY-MM-DD ~ YYYY-MM-DD
+      </button>
+      {showCalendar && <div className="date-range-picker" style={{
           "height": `${Math.floor(props.boardsNum / 3) * 292}px`,
           "gridTemplateRows": `repeat(${Math.floor(props.boardsNum / 3)}, 1fr)`,
         }}>
@@ -17,7 +23,7 @@ function App(props) {
               firstDayOfWeekIndex={props.firstDayOfWeekIndex}
               boardsNum={props.boardsNum}
           />
-      </div>
+        </div>}
     </div>
   );
 }
