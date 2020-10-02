d96b2412d4fe59321e6da508ebcc582d23edda8f
Almog Tavor <almogtavor@gmail.com>
2020-10-02 13:26:37 +0300

    Days amount container parameters update

--- src/containers/DaysAmountTabContainer/DaysAmountTabContainer.js
+++ src/containers/DaysAmountTabContainer/DaysAmountTabContainer.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux'
 import { DaysAmountTab } from "../../components/DaysAmountTabComponents/DaysAmountTab";
-import { setSelectedDays, setShowDaysAmountTab } from '../../actions';
+import { setSelectedDays, setShowDaysAmountTab, setViewedYear, setViewedMonth } from '../../actions';
 
 const mapStateToProps = (state) => ({
     selectedColor: state.lowerFooter.selectedColor,
     selectedDays: state.dayElements.selectedDays,
+    allViewedMonth: state.datesHeader.viewedMonth,
 })
 
 const mapDispatchToProps = (dispatch) => {
     return ({
         setSelectedDays: (selectedDays) => dispatch(setSelectedDays(selectedDays)),
         setShowDaysAmountTab: (showDaysAmountTab) => dispatch(setShowDaysAmountTab(showDaysAmountTab)),
+        setViewedMonth: (id, viewedMonth) =>setViewedYear(id, viewedMonth),
+        setViewedYear: (id, viewedYear) => setViewedYear(id, viewedYear),
     })
 }
 
